21c6e4d8f7826e78141058a115b714db63bb91e9
Ferrari <ferraridev18@gmail.com>
2023-05-10 17:45:21 -0300

    atividade finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
+import temaBase from './Tema/standart'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+import ResetCSS, { Container } from './styles'
+import { ThemeProvider } from 'styled-components'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={temaBase}>
+      <ResetCSS />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
-    </>
+      </Container>
+    </ThemeProvider>
   )
 }
 
---
+++ src/Tema/standart.ts
@@ -0,0 +1,11 @@
+const temaBase = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7'
+}
+
+export type Theme = {
+  corPrincipal: string
+  corSecundaria: string
+}
+
+export default temaBase
